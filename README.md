d224ca79e6bcb7f3b6023ab21613b5156ba82ef1
Thomas Weise <tweise@hfuu.edu.cn>
2021-07-31 06:56:27 +0800

    better website generation tools

--- README.md
+++ README.md
@@ -1,26 +1,52 @@
 [![make build](https://github.com/thomasWeise/bookbuilderpy/actions/workflows/build.yaml/badge.svg)](https://github.com/thomasWeise/bookbuilderpy/actions/workflows/build.yaml)
 
 # bookbuilderpy
-A python-based environment for the automated compilation of books from markdown.
+
+A python 3-based environment for the automated compilation of books from markdown.
 
 ## 1. Introduction
 
-A package for building books from markdown in Python.
+The goal of this package is to provide you with a pipeline that can:
+  - automatically compile books written in [markdown](http://pandoc.org/MANUAL.html#pandocs-markdown) to formats such as [pdf](http://thomasweise.github.io/aitoa/aitoa.pdf), stand-alone [html](http://thomasweise.github.io/aitoa/aitoa.html), 
+[epub](http://thomasweise.github.io/aitoa/aitoa.epub), and [azw3](http://thomasweise.github.io/aitoa/aitoa.azw3),
+  - support a hierarchical file structure for the book sources, i.e., allow you divide the book into chapters in folders which can contain sub-folders with sections and sub-sub-folders with sub-sections,
+  - support the automatic download and inclusion of code snippets from git repositories,
+  - allow the book to be written in multiple languages, and finally
+  - generate a website that lists all produced files so that you can copy everything to a web folder and offer your work for download without any further hassle.
+
+This python package requires that [pandoc](http://pandoc.org/), [TeX Live](http://tug.org/texlive/), and [calibre](http://calibre-ebook.com) must be installed.
+Most likely, this package will only work under Linux - at least I did not test it under Windows.
 
 ## 2. Installation
 
-You can easily install this library using `pip` by doing
+You can easily install this package and its required packages using [`pip`](https://pypi.org/project/pip/) by doing
 
 ```
 pip install git+https://github.com/thomasWeise/bookbuilderpy.git
 ```
 
-## 3. License
+## 3. Supported Markdown
+
+## 3.1. Basic Commands provided by pandoc
+
+The following basic commands are already available in pandoc's [markdown](http://pandoc.org/MANUAL.html#pandocs-markdown) flavor and/or are provided by the existing pandoc plugins that we use:
+
+- Chapters, sections, sub-sections etc. are created by putting their title on a single line preceded by `#`, `##`, or `###`, etc. For example, the line `## This is a section` starts a section with the given title. 
+- You can use LaTeX maths as inline formulae such `$\sqrt{5}+4^4$` and as stand-alone equations like `$$x=\frac{5}{6}$$`.
+
+
+## 3.2. Added Functionality
+
+The following new commands are added:
+
+- `\relative.input{path}` recursively includes and processes the contents of the file identified by `path`. `path` must be in a sub-folder of the current folder. The deepest folder of the new full path will become the current folder for any (recursive) `\relative.input{path}` invocations in the new file.  
+
+## 4. License
 
 The copyright holder of this package is Prof. Dr. Thomas Weise (see Contact).
 The package is licensed under the GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007.
 
-## 4. Contact
+## 5. Contact
 
 If you have any questions or suggestions, please contact
 [Prof. Dr. Thomas Weise](http://iao.hfuu.edu.cn/5) of the
--- bookbuilderpy/website.py
+++ bookbuilderpy/website.py
@@ -24,7 +24,25 @@ def build_website(docs: Iterable[LangResult],
                   outer_file: str,
                   body_file: Optional[str],
                   dest_dir: str,
-                  get_meta: Callable) -> File:
+                  get_meta: Callable,
+                  table_args: str = ' style="border:none;margin-left:3em"',
+                  tbody_args: str = "",
+                  lang_tr_1_args: str = '',
+                  lang_tr_2_args: str = ' style="padding-top:1em"',
+                  lang_td_bullet_args: str =
+                  ' style="text-align:right;padding-right:0;margin-right:0"',
+                  file_td_bullet_lang_args: str =
+                  ' style="text-align:right;padding-left:2em;'
+                  'padding-right:0;margin-right:0"',
+                  file_td_bullet_no_lang_args: str =
+                  ' style="text-align:right;padding-left:2em;'
+                  'padding-right:0;margin-right:0"',
+                  file_td_file_args: str = '',
+                  size_td_args: str =
+                  ' style="text-align:right;padding-right:0;'
+                  'margin-right:0;padding-left:2em"',
+                  unit_td_args: str =
+                  ' style="padding-left:0;margin-left:0"') -> File:
     """
     Build a website linking to all the generated documents.
 
@@ -33,6 +51,18 @@ def build_website(docs: Iterable[LangResult],
     :param Optional[str] body_file: the body file
     :param str dest_dir: the destination directory
     :param Callable get_meta: a callable used to get the results
+    :param str table_args: a string pasted in the table tag
+    :param str tbody_args: a string pasted in the tbody tag
+    :param str lang_tr_1_args: a string pasted into the first lang tr tag
+    :param str lang_tr_2_args: a string pasted into all following lang tr tags
+    :param str lang_td_bullet_args: a string pasted into the lang bullet td
+    :param str file_td_bullet_lang_args: a string pasted into the file bullets
+        if languages are specified
+    :param str file_td_bullet_no_lang_args: a string pasted into file bullets
+        if no language is specified
+    :param str file_td_file_args: the file path td tag parameters
+    :param str size_td_args: the size td tag parameters
+    :param str unit_td_args: the unit td tag parameters
     :return: the file record to the generated website
     :rtype: File
     """
@@ -52,59 +82,55 @@ def build_website(docs: Iterable[LangResult],
     if tag_index >= 0:
         # yes, so we load the body
         body = Path.file(body_file).read_all_str()
-        div_1 = body.find(bc.WEBSITE_BODY_TAG_1)
-        # should there be an auto-generated file list in markdown?
-        if div_1 >= 0:
-            # yes!
-            div_2 = body.find(bc.WEBSITE_BODY_TAG_2,
-                              div_1 + len(bc.WEBSITE_BODY_TAG_1))
-            if div_2 <= div_1:
-                raise ValueError(
-                    f"Website body '{body}' contains "
-                    f"'{bc.WEBSITE_BODY_TAG_1}' but not "
-                    f"'{bc.WEBSITE_BODY_TAG_2}'.")
-            data = [body[:div_1].strip(), "\n\n",
-                    '<table style="border:none;margin-left:3em">']
-
-            lang_tr = "<tr>"
-            for lang in docs:
-                if lang.lang_name:
-                    data.append(
-                        f'{lang_tr}<td style="text-align:right">&bull;'
-                        f'&nbsp;</td><td colspan="4">{lang.lang_name}'
-                        '</td></tr>')
-                    lang_tr = '<tr style="padding-top:1em">'
-                    prefix = '<tr><td /><td style="text-align:right;' \
-                             'padding-left:2em">&ndash;&nbsp;</td><td>'
-                else:
-                    prefix = '<tr><td style="text-align:right">' \
-                             '&ndash;&nbsp;</td><td>'
-
-                for res in lang.results:
-                    name = os.path.basename(res.path)
-                    if res.suffix in __SUFFIXES.keys():
-                        name = f"{__SUFFIXES[res.suffix]}&nbsp;({res.suffix})"
-                    link = res.path.relative_to(out_dir)
-                    size = file_size(res.size).replace(
-                        " ", '</td><td style="padding-left:0;'
-                             'margin-left:0">&nbsp;')
-                    data.append(
-                        f'{prefix}<a href="{link}">{name}</a></td>'
-                        '<td style="text-align:right;padding-right:0;'
-                        f'margin-right:0;padding-left:2em">{size}</td></tr>'
-                    )
-            data.append('</table>')
-            data.append(body[(div_2 + len(bc.WEBSITE_BODY_TAG_2)):].strip())
-            body = "".join(data)
-            del data
-
-        # merge the body into the outer text
         html = "\n".join(
             [html[:tag_index].strip(),
              markdown.markdown(text=body.strip(),
                                output_format="html").strip(),
              html[(tag_index + len(bc.WEBSITE_OUTER_TAG)):].strip()])
         del body
+
+    div_1 = html.find(bc.WEBSITE_BODY_TAG_1)
+    # should there be an auto-generated file list in markdown?
+    if div_1 >= 0:
+        # yes!
+        div_2 = html.find(bc.WEBSITE_BODY_TAG_2,
+                          div_1 + len(bc.WEBSITE_BODY_TAG_1))
+        if div_2 <= div_1:
+            raise ValueError(
+                f"Website '{html}' contains "
+                f"'{bc.WEBSITE_BODY_TAG_1}' but not "
+                f"'{bc.WEBSITE_BODY_TAG_2}'.")
+        data = [html[:div_1].strip(),
+                f'<table{table_args}><tbody{tbody_args}>']
+
+        lang_tr = f"<tr{lang_tr_1_args}>"
+        for lang in docs:
+            if lang.lang_name:
+                data.append(
+                    f'{lang_tr}<td{lang_td_bullet_args}>&bull;'
+                    f'&nbsp;</td><td colspan="4">{lang.lang_name}'
+                    '</td></tr>')
+                lang_tr = f'<tr{lang_tr_2_args}>'
+                prefix = '<tr><td>&nbsp;</td>' \
+                         f'<td{file_td_bullet_lang_args}>&ndash;&nbsp;' \
+                         f'</td><td{file_td_file_args}>'
+            else:
+                prefix = f'<tr><td{file_td_bullet_no_lang_args}>' \
+                         f'&ndash;&nbsp;</td><td{file_td_file_args}>'
+
+            for res in lang.results:
+                name = os.path.basename(res.path)
+                if res.suffix in __SUFFIXES.keys():
+                    name = f"{__SUFFIXES[res.suffix]}&nbsp;({res.suffix})"
+                link = res.path.relative_to(out_dir)
+                size = file_size(res.size).replace(
+                    " ", f'</td><td{unit_td_args}>&nbsp;')
+                data.append(f'{prefix}<a href="{link}">{name}</a></td>'
+                            f'<td{size_td_args}>{size}</td></tr>')
+        data.append('</tbody></table>')
+        data.append(html[(div_2 + len(bc.WEBSITE_BODY_TAG_2)):].strip())
+        html = "".join(data).strip()
+        del data
 
     html = (create_preprocessor(name=bc.CMD_GET_META,
                                 func=get_meta,
